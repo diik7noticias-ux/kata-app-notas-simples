86c0d2f7dc7d412b6fa55ef44888ee476e41eded
diik7noticias-ux <diik7noticias@gmail.com>
2026-09-18 01:51:33 +0100

    chore: atualizar codigo KATA

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,11 @@ import "package:flutter/material.dart";
 import "theme.dart";
 import "screens/home_screen.dart";
 import "screens/note_screen.dart";
+import "services/note_service.dart";
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await NoteService.init();
   runApp(const KataApp());
 }
 
@@ -20,6 +23,18 @@ class KataApp extends StatelessWidget {
       routes: {
         '/': (context) => const HomeScreen(),
         '/note': (context) => const NoteScreen(),
+        '/edit_note/:id': (context) => NoteScreen(note: context.findRootAncestorStateOfType<_HomeScreenState>()?.widget.note),
+      },
+      onGenerateRoute: (settings) {
+        if (settings.name == '/edit_note/:id') {
+          final id = settings.pathParameters?['id'];
+          if (id != null) {
+            final notes = NoteService.getAllNotes();
+            final note = notes.firstWhere((note) => note.id == id, orElse: () => Note(id: '', title: '', content: '', createdAt: DateTime.now()));
+            return MaterialPageRoute(builder: (context) => NoteScreen(note: note));
+          }
+        }
+        return null;
       },
     );
   }
