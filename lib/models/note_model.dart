0b0890b78af2daae3895d45d428c76e80e5d3c27
diik7noticias-ux <diik7noticias@gmail.com>
2026-09-18 01:50:28 +0100

    chore: adicionar codigo KATA

---
+++ lib/models/note_model.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/foundation.dart';
+
+class Note {
+  final String id;
+  final String title;
+  final String content;
+  final DateTime createdAt;
+
+  Note({
+    required this.id,
+    required this.title,
+    required this.content,
+    required this.createdAt,
+  });
+
+  Note copyWith({
+    String? id,
+    String? title,
+    String? content,
+    DateTime? createdAt,
+  }) {
+    return Note(
+      id: id ?? this.id,
+      title: title ?? this.title,
+      content: content ?? this.content,
+      createdAt: createdAt ?? this.createdAt,
+    );
+  }
+}
