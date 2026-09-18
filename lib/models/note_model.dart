92cb92b14f4c2ed9d4968e5b1bb7fd7779573b9b
diik7noticias-ux <diik7noticias@gmail.com>
2026-09-18 01:51:43 +0100

    chore: atualizar codigo KATA

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/foundation.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
+part 'note_model.g.dart';
+
+@HiveType(typeId: 0)
 class Note {
+  @HiveField(0)
   final String id;
+
+  @HiveField(1)
   final String title;
+
+  @HiveField(2)
   final String content;
+
+  @HiveField(3)
   final DateTime createdAt;
 
   Note({
