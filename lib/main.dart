29dccb914e790a73970edee2a14574d380426101
diik7noticias-ux <diik7noticias@gmail.com>
2026-09-19 01:52:00 +0100

    chore: atualizar codigo KATA

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import "package:go_router/go_router.dart";
 import "theme.dart";
 import "screens/home_screen.dart";
 import "screens/note_screen.dart";
@@ -15,27 +16,36 @@ class KataApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       title: "Notas Simples",
       debugShowCheckedModeBanner: false,
       theme: buildKataTheme(),
-      initialRoute: '/',
-      routes: {
-        '/': (context) => const HomeScreen(),
-        '/note': (context) => const NoteScreen(),
-        '/edit_note/:id': (context) => NoteScreen(note: context.findRootAncestorStateOfType<_HomeScreenState>()?.widget.note),
-      },
-      onGenerateRoute: (settings) {
-        if (settings.name == '/edit_note/:id') {
-          final id = settings.pathParameters?['id'];
+      routerConfig: _router,
+    );
+  }
+
+  final GoRouter _router = GoRouter(
+    routes: [
+      GoRoute(
+        path: '/',
+        builder: (context, state) => const HomeScreen(),
+      ),
+      GoRoute(
+        path: '/note',
+        builder: (context, state) => const NoteScreen(),
+      ),
+      GoRoute(
+        path: '/edit_note/:id',
+        builder: (context, state) {
+          final id = state.pathParameters['id'];
           if (id != null) {
             final notes = NoteService.getAllNotes();
             final note = notes.firstWhere((note) => note.id == id, orElse: () => Note(id: '', title: '', content: '', createdAt: DateTime.now()));
-            return MaterialPageRoute(builder: (context) => NoteScreen(note: note));
+            return NoteScreen(note: note);
           }
-        }
-        return null;
-      },
-    );
-  }
+          return const NoteScreen();
+        },
+      )
+    ],
+  );
 }
